c4a7a09396e779c407a93548a18032286311c5a7
Nicolas Hery <nicolahery@gmail.com>
2013-03-19 13:20:18 -0400

    Throw err instead of passing in cb for widget#update

--- producers/index.js
+++ producers/index.js
@@ -1,5 +1,7 @@
 var ProducerCollection = require('../lib/producer_collection');
 
+// Warning: make sure you set this env variable on Heroku, 
+// or app will crash trying to post to localhost
 var httpEndpoint = process.env.PRODUCER_HTTPENDPOINT || 'http://localhost:3000/channels';
 
 var producers = module.exports = new ProducerCollection({
